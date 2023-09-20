3f443e8c642c0c4292a72f1a83026077340ccf51
GreenH <huangshixin47@gmail.com>
2023-09-20 13:48:54 +1000

    a3

--- 5120/Policy.sql
+++ 5120/Policy.sql
@@ -16,3 +16,18 @@ select c.postcode, p.council_name,p.waste_type, p.waste_policy
 from Policy p,Postcode c
 where p.council_name = c.council_name
 and c.postcode = '3352';
+
+select distinct Policy.council_name
+from Policy;
+
+
+select *
+from Postcode
+where council_name = 'Alpine Council';
+
+-- check how many postcode related with policy
+SELECT p.council_name,c.postcode
+FROM Policy p
+JOIN Postcode c ON p.council_name = c.council_name
+GROUP BY p.council_name,c.postcode
+
